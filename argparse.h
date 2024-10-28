8f2d4af1a2e219807f5d895c7854abbc3fa56259
Calluna <106458302+CallunaLessThan3@users.noreply.github.com>
2024-10-28 00:11:39 -0700

    update argcount definition

--- argparse.h
+++ argparse.h
@@ -4,9 +4,10 @@
 /*Argparse
 * line        the input string that contains arguments seperated by whitespaces
 * arggcp      A int pointer that the count of the amount of arguments will be stored
-* returns a array of char*'s each of which is a argument 
+* returns a array of char*'s each of which is a argument
 */
 char** argparse(char* line, int* argcp);
+static int argCount(char* line);
 
 #endif
 
